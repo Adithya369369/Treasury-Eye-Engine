d4da7ff9e93e99ca3013799b7ccca079b17d4972
Adithyaxrx <Adithya369963@gmail.com>
2025-09-14 11:56:20 -0400

    First Commit

---
+++ Tfg-Engine/src/main/java/SpringBoot/App/Tfg_Engine/TfgEngineApplication.java
@@ -0,0 +1,13 @@
+package SpringBoot.App.Tfg_Engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgEngineApplication.class, args);
+        System.out.println("Hello World");
+	}
+}
---
+++ Tfg-Engine/src/test/java/SpringBoot/App/Tfg_Engine/TfgEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.App.Tfg_Engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
